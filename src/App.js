79277488482e842b95ced6c404bc216e090c9ec3
Allan <kipropsallan@gmail.com>
2022-07-27 01:19:52 +0300

    Modified to include react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css';
+import Coin from './Coin';
+import { Container } from 'react-bootstrap';
+import {blue} from "@material-ui/core/colors";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('')
+  useEffect(()=>{
+    fetch('https://api.coincap.io/v2/assets')
+    .then(res=>{
+      setCoins(res.data)
+      console.log(res.data)
+    }).catch(error=> console.error(error))
+  }, [])
+
+  const handleChange = e =>{
+    setSearch(e.target.value);
+  }
+ 
+  const filteredCoins= coins.filter(coin =>
+    coins.name().inludes(search)
+    )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="coin-search">
+
+    </div>
+
+    {filteredCoins.map(coin=>{
+    return(
+      <Coin
+      key={coin.id}
+      name={coin.name}
+      image={coin.image}
+      symbol={coin.symbol}
+      pricechange={coin.price_change_perentage_24h}
+      />
+    );
+    })}
     </div>
   );
 }
