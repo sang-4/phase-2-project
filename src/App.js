13cea201e191d1d22c2972eb6ebf0ae5de88b58a
Allan Kiprop <81680143+sang-4@users.noreply.github.com>
2022-07-28 14:35:59 +0300

    Revert "Updates"

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Coin from './Coin';
+
+
+function App() {
+  const [coins,setCoins] = useState([])
+  const [search,setSearch] = useState('')
+  useEffect(() => {
+    fetch(' http://localhost:3000/crypto')
+    .then(res=> res.json())
+    .then(data =>setCoins(data))
+    .catch(error=>console.log(error))
+  }, [])
+  const handleChange = e =>{
+    setSearch(e.target.value)
+  }
+  const filteredCoins = coins.filter(coin=> coin.name.toLowerCase().includes(search.toLowerCase()
+    )
+    )
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">CryptIt</h1>
+        <form action="">
+          <input type="text" className="coin-input" placeholder="Enter your preffered asset name" onChange={handleChange}/>
+
+        </form>
+
+      </div>
+      {filteredCoins.map(coin=>{
+        return(
+          <Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image} 
+          symbol={coin.symbol}
+          marketcap={coin.market_cap}
+          price={coin.current_price}
+          pricechange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
+
+
+    </div>
+  );
+}
+
+export default App;
